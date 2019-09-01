2b18ebfbc3096afb9f34018e117c27ef8a99c5e2
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-08-31 20:30:41 -0400

    added Jquery link to html. Pseudocoded in the Javascript file everything i believe I need to create a working game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//create variables for everything that needs to be updated on-screen.
+//createa random target score for each game.
+//give value to each crystal.
+//create onclicks that respond to the click of each crystal.
+//create counters for each crystal that corresponds to the value of each crystal.
+//create win and loss logic and have the result print to either wins or losses.
+//create logic that resets the game each time someone wins or loses.
